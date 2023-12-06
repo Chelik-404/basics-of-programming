0429330c3fa95e42a03ee6cf406e742cea59448e
Chelik-404 <workchelik@gmail.com>
2023-12-06 21:09:31 +0300

    Fix

--- Работа 2/src/BuildingFactory.java
+++ Работа 2/src/BuildingFactory.java
@@ -1,5 +1,39 @@
-// Абстрактный класс BuildingFactory, который определяет общий интерфейс для создания объектов разных типов зданий
-abstract class BuildingFactory {
-    // Абстрактный метод, который принимает адрес, площадь и дополнительный параметр и возвращает объект здания
-    public abstract Building createBuilding(String address, double area, int parameter);
+// Класс BuildingFactory, который использует паттерн одиночка для создания объектов разных типов зданий
+class BuildingFactory {
+    // Статическая переменная, которая хранит единственный экземпляр класса factory
+    private static BuildingFactory instance;
+
+    // Приватный конструктор, который запрещает создание объектов класса factory извне
+    private BuildingFactory() {
+    }
+
+    // Статический метод, который возвращает единственный экземпляр класса factory, создавая его при необходимости
+    public static BuildingFactory getInstance() {
+        // Если экземпляр класса factory еще не создан, то создать его
+        if (instance == null) {
+            instance = new BuildingFactory();
+        }
+        // Вернуть экземпляр класса factory
+        return instance;
+    }
+
+    // Метод, который принимает тип, адрес, площадь и дополнительный параметр и возвращает объект здания
+    public Building createBuilding(String type, String address, double area, int parameter) {
+        // Если тип здания - жилой, то создать и вернуть объект класса ResidentialBuilding
+        if (type.equalsIgnoreCase("residential")) {
+            return new ResidentialBuilding(address, area, parameter);
+        }
+        // Если тип здания - коммерческий, то создать и вернуть объект класса CommercialBuilding
+        else if (type.equalsIgnoreCase("commercial")) {
+            return new CommercialBuilding(address, area, parameter);
+        }
+        // Если тип здания - промышленный, то создать и вернуть объект класса IndustrialBuilding
+        else if (type.equalsIgnoreCase("industrial")) {
+            return new IndustrialBuilding(address, area, parameter);
+        }
+        // Иначе вернуть null
+        else {
+            return null;
+        }
+    }
 }
--- Работа 2/src/Main.java
+++ Работа 2/src/Main.java
@@ -2,20 +2,17 @@
 class Main {
     // Главный метод, который демонстрирует работу абстрактной фабрики
     public static void main(String[] args) {
-        // Создать объекты разных типов фабрик зданий
-        BuildingFactory residentialBuildingFactory = new ResidentialBuildingFactory();
-        BuildingFactory commercialBuildingFactory = new CommercialBuildingFactory();
-        BuildingFactory industrialBuildingFactory = new IndustrialBuildingFactory();
+        // Получить единственный экземпляр класса BuildingFactory
+        BuildingFactory buildingFactory = BuildingFactory.getInstance();
         // Создать объект класса City
         City city = new City();
-        // Добавить в город здания разных типов с помощью абстрактной фабрики
-        city.addBuilding(residentialBuildingFactory.createBuilding("ул. Ленина, 1", 1000, 50));
-        city.addBuilding(commercialBuildingFactory.createBuilding("пр. Мира, 10", 2000, 20));
-        city.addBuilding(industrialBuildingFactory.createBuilding("ул. Заводская, 5", 3000, 10));
-        city.addBuilding(residentialBuildingFactory.createBuilding("ул. Садовая, 15", 1500, 40));
-        city.addBuilding(commercialBuildingFactory.createBuilding("пр. Победы, 20", 2500, 30));
-        city.addBuilding(industrialBuildingFactory.createBuilding("ул. Промышленная, 7", 3500, 15));
-        city.addBuilding(residentialBuildingFactory.createBuilding("ул. Высотского, 69", 2998, 12));
+        // Добавить в город здания разных типов с помощью одиночной фабрики
+        city.addBuilding(buildingFactory.createBuilding("residential", "ул. Ленина, 1", 1000, 50));
+        city.addBuilding(buildingFactory.createBuilding("commercial", "пр. Мира, 10", 2000, 20));
+        city.addBuilding(buildingFactory.createBuilding("industrial", "ул. Заводская, 5", 3000, 10));
+        city.addBuilding(buildingFactory.createBuilding("residential", "ул. Садовая, 15", 1500, 40));
+        city.addBuilding(buildingFactory.createBuilding("commercial", "пр. Победы, 20", 2500, 30));
+        city.addBuilding(buildingFactory.createBuilding("industrial", "ул. Промышленная, 7", 3500, 15));
         // Вывести на экран информацию о городе
         System.out.println("В городе " + city.getBuildings().size() + " зданий");
         System.out.println("Общая площадь всех зданий: " + city.getTotalArea() + " кв.м");
